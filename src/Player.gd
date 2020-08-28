480f6ed2c1878d2807aa9873e73a30a76a7e6514
Adam Pendleton <adam@Adams-MBP.cinci.rr.com>
2020-08-28 16:32:30 -0400

    added player movement

---
+++ src/Player.gd
@@ -0,0 +1,57 @@
+extends KinematicBody2D
+export var gravity: int = 10
+export var max_fall_speed: int = 1000
+export var accel: int = 100
+export var max_max_speed: int = 200
+export var friction: float = 0.7
+export var jump_strength: int = 300
+
+var velocity: Vector2
+var facing_left: bool = true
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+	
+func update_sprite():
+	if velocity.x < 0:
+		facing_left = true
+	elif velocity.x > 0:
+		facing_left = false
+	if facing_left:
+		$Sprite.flip_h = true
+	else: 
+		$Sprite.flip_h = false
+	
+func _physics_process(delta: float) -> void:
+	var new_velocity = get_new_velocity()
+	velocity = move_and_slide(new_velocity, Vector2.UP)
+	update_sprite()
+
+func get_new_velocity() -> Vector2:
+	var new_velocity: Vector2 = velocity
+	new_velocity = add_gravity(new_velocity)
+	new_velocity = add_player_input(new_velocity)
+	new_velocity = add_friction(new_velocity)
+	return new_velocity
+
+func add_player_input(given_velocity: Vector2) -> Vector2:
+	if Input.get_action_strength("right") > 0:
+		given_velocity.x += accel
+	elif Input.get_action_strength("left") > 0:
+		given_velocity.x -= accel
+	if Input.get_action_strength("up") > 0 and is_on_floor():
+		given_velocity.y = -jump_strength
+	
+	return given_velocity
+	
+func add_gravity(given_velocity: Vector2) -> Vector2:
+	if given_velocity.y > max_fall_speed:
+		given_velocity.y = max_fall_speed
+	else:
+		given_velocity.y += gravity
+	return given_velocity
+	
+func add_friction(given_velocity: Vector2) -> Vector2:
+	given_velocity.x = given_velocity.x * friction
+	return given_velocity

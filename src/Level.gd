28ee649edf3006c5fc85718adecf08ad4943687f
Adam Pendleton <adam@Adams-MBP.cinci.rr.com>
2020-08-28 19:11:51 -0400

    infinite climbing

--- src/Level.gd
+++ src/Level.gd
@@ -1,31 +1,45 @@
 extends Node2D
 
 onready var rng := RandomNumberGenerator.new()
+onready var player := get_node("/root/Game/Player")
+export var stage_forgetting_distance = 900
+export var stage_spawn_distance = 600
+
+var highest_stage = null
 var templates_count = 2
 
 func _ready() -> void:
-	var position: Vector2 = $Player.position
-	for i in range(1, 10):
-		generate_level_chunk(position)
-		#TODO: each chunk should be able to have its own height, so move the position based on the height of the last chunk
-		position.y -= 120
-		
-	#TODO: generate new chunks as the player reaches further up
-	#also, remove old chunks to keep from accumulating too many children
+	create_initial_level()
 	
+func create_initial_level() -> void:
+	var spawn_position: Vector2 = player.position
+	spawn_position.y += 50
+	highest_stage = generate_level_stage(spawn_position)
+	while player.position.distance_to(highest_stage.position) < stage_spawn_distance:
+		highest_stage = generate_level_stage(spawn_position)
+		#TODO: each stage should be able to have its own height
+		spawn_position.y -= highest_stage.height
+		
 func _process(delta) -> void:
-	pass
+	for stage in get_children():
+		if player.position.y < stage.position.y and player.position.distance_to(stage.position) > stage_forgetting_distance:
+			stage.call_deferred("free")
+	
+	if player.position.distance_to(highest_stage.position) < stage_spawn_distance:
+		var new_stage = generate_level_stage(highest_stage.position - Vector2(0, highest_stage.height))
+		highest_stage = new_stage
 
-func generate_level_chunk(position: Vector2) -> void:
+func generate_level_stage(position: Vector2) -> Node:
 	var template = get_level_template_scene()
-	var chunk = template.instance()
-	add_child(chunk)
-	chunk.position = position
+	var stage = template.instance()
+	add_child(stage)
+	stage.position = position
+	return stage
 	
 func get_level_template_scene() -> Resource:
 	rng.randomize()
 	var random_number = rng.randi_range(1, templates_count)
 	var level_path = "res://src/Levels/LevelTemplate%s.tscn"
 	level_path = level_path % random_number
-	var level_chunk: Resource = load(level_path)
-	return level_chunk
+	var level_stage: Resource = load(level_path)
+	return level_stage

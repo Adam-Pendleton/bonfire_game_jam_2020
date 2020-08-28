d0d390822e8b6a15c48b7e6d5114fb2c2858d3d2
Adam Pendleton <adam@Adams-MBP.cinci.rr.com>
2020-08-28 18:20:43 -0400

    generate a simple level based on the templates

--- src/Level.gd
+++ src/Level.gd
@@ -4,16 +4,23 @@ onready var rng := RandomNumberGenerator.new()
 var templates_count = 2
 
 func _ready() -> void:
-	generate_level_chunk()
+	var position: Vector2 = $Player.position
+	for i in range(1, 10):
+		generate_level_chunk(position)
+		#TODO: each chunk should be able to have its own height, so move the position based on the height of the last chunk
+		position.y -= 120
+		
+	#TODO: generate new chunks as the player reaches further up
+	#also, remove old chunks to keep from accumulating too many children
 	
 func _process(delta) -> void:
 	pass
 
-func generate_level_chunk() -> void:
+func generate_level_chunk(position: Vector2) -> void:
 	var template = get_level_template_scene()
 	var chunk = template.instance()
 	add_child(chunk)
-	chunk.position = $Player.get_global_position()
+	chunk.position = position
 	
 func get_level_template_scene() -> Resource:
 	rng.randomize()

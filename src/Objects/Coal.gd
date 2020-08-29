00cfbaca9097acaca4791b326f2625e356f7fa00
Michael Benedict <thebenedict@gmail.com>
2020-08-28 17:05:00 -0700

    collect coins

---
+++ src/Objects/Coal.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
+
+func _on_body_entered(body: KinematicBody2D) -> void:
+	anim_player.play("fade_out")
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	call_deferred("free")

cffb63e434f2f05ec4e2f256fe3721c32e60c3bd
Adam Pendleton <pendla10@gmail.com>
2020-08-29 17:29:52 -0400

    fonts and other nonsense

--- src/Game.gd
+++ src/Game.gd
@@ -14,11 +14,18 @@ func _process(delta: float) -> void:
 	if $Player.dead and not game_over:
 		game_over = true
 		show_game_over_screen()
+
+func level_complete() -> void:
+	show_complete_screen()
+	restart_level()
+		
+func show_complete_screen() -> void:
+	var complete_screen = preload("res://src/Screens/CompleteScreen.tscn").instance()
+	add_child(complete_screen)
+	
 		
 func show_game_over_screen() -> void:
 	var game_over_screen = preload("res://src/Screens/GameOverScreen.tscn").instance()
-	var texture = game_over_screen.get_node("TextureRect")
-	texture.rect_size = get_viewport().size
 	add_child(game_over_screen)
 
 func restart_level() -> void:

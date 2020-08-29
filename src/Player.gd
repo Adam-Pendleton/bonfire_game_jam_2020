8de0875aefb94021cd8218c00ffda7b0296feac6
Adam Pendleton <pendla10@gmail.com>
2020-08-29 01:01:00 -0400

    tweak jumping and add double jump

--- src/Player.gd
+++ src/Player.gd
@@ -1,10 +1,12 @@
 extends KinematicBody2D
-export var gravity: int = 30
+export var gravity: int = 35
 export var max_fall_speed: int = 1000
 export var accel: int = 100
-export var max_max_speed: int = 200
-export var friction: float = 0.2
-export var jump_strength: int = 500
+export var max_speed: int = 200
+export var friction: float = 0.3
+export var air_resistance: float = 0.2
+export var air_accel: float = 70
+export var jump_strength: int = 660
 
 var jump_count = 0
 var velocity: Vector2
@@ -28,6 +30,8 @@ func _physics_process(delta: float) -> void:
 	if dead:
 		return
 	var new_velocity = get_new_velocity()
+	if is_on_floor():
+		jump_count = 0
 	velocity = move_and_slide(new_velocity, Vector2.UP)
 	check_if_dead()
 	update_sprite()
@@ -40,22 +44,33 @@ func check_if_dead() -> void:
 
 func get_new_velocity() -> Vector2:
 	var new_velocity: Vector2 = velocity
-	new_velocity = add_gravity(new_velocity)
 	new_velocity = add_player_input(new_velocity)
-	new_velocity = add_friction(new_velocity)
+	new_velocity = add_gravity(new_velocity)
+	if is_on_floor():
+		new_velocity = add_friction(new_velocity)
+	else:
+		new_velocity = add_air_resistance(new_velocity)
+		
 	return new_velocity
 
 func add_player_input(given_velocity: Vector2) -> Vector2:
 	if Input.get_action_strength("right") > 0:
-		given_velocity.x += accel
+		if is_on_floor():
+			given_velocity.x += accel
+		else:
+			given_velocity.x += air_accel
 	elif Input.get_action_strength("left") > 0:
-		given_velocity.x -= accel
+		if is_on_floor():
+			given_velocity.x -= accel
+		else:
+			given_velocity.x -= air_accel
 	
-	if Input.get_action_strength("up") > 0 and is_on_floor():
+	if Input.is_action_just_pressed("up") and jump_count < 1:
 		given_velocity.y = -jump_strength
+		jump_count += 1
 		
-	if Input.is_action_just_released("up") and given_velocity.y < -150:
-		given_velocity.y = -150
+	if Input.is_action_just_released("up") and given_velocity.y < -jump_strength / 2:
+		given_velocity.y = -jump_strength / 2
 	
 	return given_velocity
 	
@@ -68,4 +83,8 @@ func add_gravity(given_velocity: Vector2) -> Vector2:
 	
 func add_friction(given_velocity: Vector2) -> Vector2:
 	given_velocity.x = lerp(given_velocity.x, 0, friction)
+	return given_velocity
+
+func add_air_resistance(given_velocity: Vector2) -> Vector2:
+	given_velocity.x = lerp(given_velocity.x, 0, air_resistance)
 	return given_velocity

746c93029dffe8867849c0e9402c84d3891e6631
Michael Benedict <thebenedict@gmail.com>
2020-08-29 17:50:49 -0700

    tentacles behind platforms

--- src/Bank.gd
+++ src/Bank.gd
@@ -12,6 +12,11 @@ func _physics_process(delta: float) -> void:
 		return
 	position.y -= speed
 	speed += speed * acceleration_factor
+	sync_background(position, speed)
 
 func reset_speed() -> void:
 	speed = starting_speed
+	
+func sync_background(position: Vector2, speed: float) -> void:
+	var background = get_node("/root/Game/BankBackground")
+	background.position = position
